75c9315b7460a79f98feeed766df40d2b9844a6f
Alexander Xavier Moreira <alexkurumin@gmail.com>
2020-08-17 19:39:26 -0300

    Pagina da Turma Montese

---
+++ turma-montese/assets/js/script.js
@@ -0,0 +1,19 @@
+// Função para carregar a página antes de executar o jQuery
+// jQuery ==> Evitar conflito com outra variável
+jQuery(document).ready(function() {
+    console.log("PÁGINA CARREGADA");
+});
+// Função para carregar a página antes de executar o jQuery
+
+$(function(){
+    $('.menu_hamburguer').bind('click', function(){
+        $('.sobreMenu').animate({
+            'left':0
+        },500);
+    });
+    $('.fecharSobreMenu').bind('click', function(){
+        $('.sobreMenu').animate({
+            'left':-2000
+        },500);
+    });
+});
